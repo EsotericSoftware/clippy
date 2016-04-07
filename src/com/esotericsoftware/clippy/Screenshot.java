6abda6d244497451e85deb78520ebfe98dd680cf
NathanSweet <nathan.sweet@gmail.com>
2016-04-07 14:27:22 +0200

    Fixed region screenshot accuracy.

--- src/com/esotericsoftware/clippy/Screenshot.java
+++ src/com/esotericsoftware/clippy/Screenshot.java
@@ -28,7 +28,6 @@ import java.awt.event.MouseMotionAdapter;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Ellipse2D.Float;
 import java.awt.image.BufferedImage;
 import java.awt.image.VolatileImage;
 import java.io.File;
@@ -48,7 +47,7 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class Screenshot {
-	static final int mag = 8, magOffsetX = 8, magOffsetY = 8, magW = 280, magH = 280;
+	static final int mag = 8, magOffsetX = 20, magOffsetY = 20, magD = 280;
 	static final int crosshair = 16;
 
 	final Clippy clippy = Clippy.instance;
@@ -128,8 +127,9 @@ public class Screenshot {
 
 		JFrame frame = new JFrame() {
 			int lastX, lastY, x, y, x1, y1;
-			int magX = magOffsetX, magY = magOffsetY;
+			int magX = -(magD + magOffsetX), magY = -(magD + magOffsetY);
 			boolean drag, lock, lockX, robotEvent;
+			final Ellipse2D.Float circle = new Ellipse2D.Float(0, 0, magD, magD);
 
 			{
 				Point mouse = MouseInfo.getPointerInfo().getLocation();
@@ -186,6 +186,22 @@ public class Screenshot {
 						else if (y > height) //
 							y = height;
 
+						if (drag) {
+							if (x1 < x && y1 < y) {
+								magX = magOffsetX;
+								magY = magOffsetY;
+							} else if (x1 > x && y1 > y) {
+								magX = -(magD + magOffsetX);
+								magY = -(magD + magOffsetY);
+							} else if (x1 < x && y1 > y) {
+								magX = magOffsetX;
+								magY = -(magD + magOffsetY);
+							} else if (x1 > x && y1 < y) {
+								magX = -(magD + magOffsetX);
+								magY = magOffsetY;
+							}
+						}
+
 						repaint();
 
 						// Prevent mouse from getting stuck against screen edge.
@@ -200,8 +216,8 @@ public class Screenshot {
 						lastX = e.getX();
 						lastY = e.getY();
 						drag = true;
-						x1 = x;
-						y1 = y;
+						x1 = x - 1;
+						y1 = y - 1;
 					}
 
 					public void mouseReleased (MouseEvent e) {
@@ -237,6 +253,10 @@ public class Screenshot {
 					};
 				});
 				addWindowListener(new WindowAdapter() {
+					public void windowDeactivated (WindowEvent e) {
+						dispose();
+					};
+
 					public void windowClosed (WindowEvent e) {
 						robot.mouseMove(x, y);
 					}
@@ -246,14 +266,15 @@ public class Screenshot {
 			public void paint (Graphics graphics) {
 				Graphics2D g = (Graphics2D)graphics;
 
+				// Keep within screen bounds.
 				if (x + magX < 0)
 					magX = magOffsetX;
-				else if (x + magX + magW > width) //
-					magX = -(magW + magOffsetX);
+				else if (x + magX + magD > width) //
+					magX = -(magD + magOffsetX);
 				if (y + magY < 0)
 					magY = magOffsetY;
-				else if (y + magY + magH > height) //
-					magY = -(magH + magOffsetY);
+				else if (y + magY + magD > height) //
+					magY = -(magD + magOffsetY);
 
 				// Draw whole screen image.
 				g.drawImage(image, 0, 0, width, height, null);
@@ -264,18 +285,19 @@ public class Screenshot {
 				}
 
 				// Magnified image.
-				Float circle = new Ellipse2D.Float(x + magX, y + magY, magW, magH);
+				circle.x = x + magX;
+				circle.y = y + magY;
 				g.setClip(circle);
 				g.drawImage(image, //
-					x + magX + mag, y + magY + mag, x + magX + magW, y + magY + magH, //
-					x - magW / (mag * 2), y - magH / (mag * 2), x + magW / (mag * 2), y + magW / (mag * 2), null);
+					x + magX, y + magY, x + magX + magD, y + magY + magD, //
+					x - (magD + mag) / (mag * 2), y - (magD + mag) / (mag * 2), x + magD / (mag * 2), y + magD / (mag * 2), null);
 				g.setClip(null);
 
 				// Magnified crosshair.
 				int space = mag * 2;
-				int cx = x + magX + magW / 2 - mag / 2;
+				int cx = x + magX + magD / 2 - mag / 2;
 				int cy = y + magY + space;
-				int ch = magH / 2 - mag * 3 / 2 - space;
+				int ch = magD / 2 - mag * 3 / 2 - space;
 				g.setColor(Color.black);
 				g.fillRect(cx, cy, mag, ch);
 				g.fillRect(cx, cy + ch + mag * 3, mag, ch);
@@ -285,8 +307,8 @@ public class Screenshot {
 				g.drawRect(cx, cy, mag + 1, ch + 1);
 				g.drawRect(cx, cy + ch + mag * 3, mag + 1, ch + 1);
 				cx = x + magX + space;
-				cy = y + magY + magH / 2 - mag / 2;
-				int cw = magW / 2 - mag * 3 / 2 - space;
+				cy = y + magY + magD / 2 - mag / 2;
+				int cw = magD / 2 - mag * 3 / 2 - space;
 				g.setColor(Color.black);
 				g.fillRect(cx, cy, cw, mag);
 				g.fillRect(cx + cw + mag * 3, cy, cw, mag);
