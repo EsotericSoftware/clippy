9bdfd63f56aa3093baa5f2a647b05a3279e58da6
NathanSweet <nathan.sweet@gmail.com>
2016-08-14 11:22:20 +0200

    Protect against setting gamme too low. Restore full gamma on shutdown. Fixed brightness being ignored. Fixed specifying only a single time.

--- src/com/esotericsoftware/clippy/Gamma.java
+++ src/com/esotericsoftware/clippy/Gamma.java
@@ -19,6 +19,7 @@ import com.esotericsoftware.clippy.util.Util;
 public class Gamma {
 	static private final float maxNoticeableChange = 0.005f;
 	static private final int minSeconds = 1, maxSeconds = 60;
+	static private final float minGamma = 0.25f;
 
 	final Clippy clippy = Clippy.instance;
 	ArrayList<GammaTime> times;
@@ -32,6 +33,12 @@ public class Gamma {
 		Collections.sort(times, new Comparator<GammaTime>() {
 			public int compare (GammaTime o1, GammaTime o2) {
 				return o1.daySecond - o2.daySecond;
+			}
+		});
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run () {
+				set(1, 1, 1, 1, true);
 			}
 		});
 
@@ -59,8 +66,11 @@ public class Gamma {
 		int from = fromTime.daySecond, to = toTime.daySecond;
 		if (from > to) to += 24 * 60 * 60;
 		float duration = to - from;
-		float a = (current - from) / duration, ia = 1 - a;
-		set(fromTime.r + dr * a, fromTime.g + dg * a, fromTime.b + db * a, fromTime.brightness + dbrightness * a, false);
+		if (duration > 0.0001f) {
+			float a = (current - from) / duration, ia = 1 - a;
+			set(fromTime.r + dr * a, fromTime.g + dg * a, fromTime.b + db * a, fromTime.brightness + dbrightness * a, false);
+		} else
+			set(toTime.r, toTime.g, toTime.b, toTime.brightness, false);
 
 		float maxChange = Math.max(dr, dg);
 		maxChange = Math.max(maxChange, db);
@@ -74,9 +84,29 @@ public class Gamma {
 		}, seconds * 1000);
 	}
 
-	void set (float r, float g, float b, float brightness, boolean force) {
+	void set (float red, float green, float blue, float brightness, boolean force) {
+		float r = red * brightness;
+		float g = green * brightness;
+		float b = blue * brightness;
+
+		float total = r + g + b;
+		if (Float.isNaN(total) || Float.isInfinite(total)) {
+			r = 1;
+			g = 1;
+			b = 1;
+			if (ERROR) error("Invalid gamma: " + red + ", " + green + ", " + blue + " * " + brightness);
+		} else if (total < 0.001f) {
+			r = minGamma / 3;
+			g = minGamma / 3;
+			b = minGamma / 3;
+		} else if (total < minGamma) {
+			r = r / total * minGamma;
+			g = g / total * minGamma;
+			b = b / total * minGamma;
+		}
+
 		if (Math.abs(r - this.r) <= 0.001f && Math.abs(g - this.g) <= 0.001f && Math.abs(b - this.b) <= 0.001f) return;
-		if (TRACE) trace("Gamma: " + r + ", " + g + ", " + b);
+		if (TRACE) trace("Gamma: " + red + ", " + green + ", " + blue + " * " + brightness);
 		this.r = r;
 		this.g = g;
 		this.b = b;
@@ -87,7 +117,7 @@ public class Gamma {
 			ramp.Blue[i] = (char)(i * (b * 256));
 		}
 		if (!Gdi32.SetDeviceGammaRamp(User32.GetDC(null), ramp)) {
-			if (DEBUG) debug("Unable to set gamma ramp.");
+			if (DEBUG) debug("Unable to set gamma ramp: " + red + ", " + green + ", " + blue + " * " + brightness);
 		}
 	}
 }
